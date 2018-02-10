269dc3270e38b86cb22717fa97afa5df3d7c968f
https://github.com/settings/profile <shashank.agrawal@onmobile.com>
2018-02-10 18:55:57 -0500

    project structure

---
+++ src/main/java/co/sportiz/backend/SportizBackendApplication.java
@@ -0,0 +1,12 @@
+package co.sportiz.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportizBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportizBackendApplication.class, args);
+	}
+}
---
+++ src/main/java/co/sportiz/backend/controller/ContextMenuController.java
@@ -0,0 +1,15 @@
+package co.sportiz.backend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ContextMenuController {
+	
+	@RequestMapping("/")
+	public @ResponseBody String home() {
+		return "Hello World!";
+	}
+
+}
