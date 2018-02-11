7e6eb347999439829b7f062ead5a792931aeabd7
https://github.com/settings/profile <shashank.agrawal@onmobile.com>
2018-02-10 21:43:06 -0500

    Updated request mapping

--- src/main/java/co/sportiz/backend/controller/ContextMenuController.java
+++ src/main/java/co/sportiz/backend/controller/ContextMenuController.java
@@ -4,7 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller("context")
+@Controller
+@RequestMapping("/context/")
 public class ContextMenuController {
 	
 	@RequestMapping("/")
