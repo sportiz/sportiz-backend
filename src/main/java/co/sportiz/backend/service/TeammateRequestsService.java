64c507fa235f3ca9177f60ca645480bb3048407f
https://github.com/settings/profile <shashank.agrawal@onmobile.com>
2018-02-16 21:35:42 -0500

    refactor code for fetching requests

--- src/main/java/co/sportiz/backend/service/TeammateRequestsService.java
+++ src/main/java/co/sportiz/backend/service/TeammateRequestsService.java
@@ -1,12 +1,9 @@
 package co.sportiz.backend.service;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.google.common.collect.Iterables;
 
 import co.sportiz.backend.model.TeammateRequests;
 import co.sportiz.backend.repository.ContextMenuRepo;
@@ -22,13 +19,9 @@ public class TeammateRequestsService {
 	private ContextMenuRepo contextMenuRepo;
 	
 	public List<TeammateRequests> fetchRequestsForSports(String sport) {
-		if(sport != null) {
-			return teammateRequestsRepo.findBySport(sport);
-		} else {
-			String sportName = contextMenuRepo.findAll().iterator().next().getValue();
-			System.out.println(sportName);
-			return Arrays.asList(Iterables.toArray(teammateRequestsRepo.findBySport(sportName), TeammateRequests.class));
-		}
+		if(sport == null)
+			sport = contextMenuRepo.findAll().iterator().next().getValue();
+		return teammateRequestsRepo.findBySport(sport);
 	}
 
 }
