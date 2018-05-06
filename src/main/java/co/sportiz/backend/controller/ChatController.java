780cc9f000768e6dc98f8249f18d0071dc1ad75b
https://github.com/settings/profile <shashank.agrawal@onmobile.com>
2018-05-05 20:57:27 -0400

    Removed logs

--- src/main/java/co/sportiz/backend/controller/ChatController.java
+++ src/main/java/co/sportiz/backend/controller/ChatController.java
@@ -29,8 +29,8 @@ public class ChatController {
     public String sendMessage(@RequestBody Message message, Principal principal) {
     	Map<String, Object> map = new HashMap<>();
 		map.put(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.APPLICATION_JSON);
-		this.messagingTemplate.convertAndSendToUser(message.getReciver(), QUEUE_PATH_LOCATOR, message.getContent());
 		System.out.println("Message pushed to queue for user "+(principal!=null? principal.getName() : "User is null")+" with content as "+message.getContent());
+		this.messagingTemplate.convertAndSendToUser(message.getReciver(), QUEUE_PATH_LOCATOR, message.getContent());
 		return "SUCCESS";
     }
 	
